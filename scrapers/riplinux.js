6efe8b7ce0382006dacb0d93fa2e060c510389bc
FrozenCow <frozencow@gmail.com>
2013-03-01 18:18:20 +0100

    Added RIPLinuX

---
+++ scrapers/riplinux.js
@@ -0,0 +1,26 @@
+var request = require('../request.js');
+var async = require('async');
+var sugar = require('sugar');
+var url = require('url');
+
+module.exports = function(callback) {
+	var distribution = {
+		id: 'riplinux',
+		name: 'RIPLinuX',
+		url: 'http://www.tux.org/pub/people/kent-robotti/looplinux/rip/'
+	};
+	request.dom(distribution.url,function(err,$,response) {
+		// Webpage not in valid HTML, so regexp the source.
+		var result = /"(RIPLinuX-(\d+(?:\.\d+)*)\.iso)"/.exec(response.body);
+		var release = {
+			url: url.resolve(distribution.url, result[1]),
+			version: result[2]
+		};
+		request.contentlength(release.url,function(err,contentlength) {
+			if (err) { return callback(err); }
+			release.size = contentlength;
+			distribution.releases = [release];
+			callback(null,distribution);
+		});
+	});
+};
