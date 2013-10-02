ae778b237c3c67e5c037efa4d6df88dcd7fe9177
FrozenCow <frozencow@gmail.com>
2013-10-02 20:45:54 +0200

    Updated kalilinux to work with new directory structure

--- scrapers/kalilinux.js
+++ scrapers/kalilinux.js
@@ -4,35 +4,53 @@ var sugar = require('sugar');
 var URL = require('url');
 
 module.exports = function(callback) {
-	var distribution = {
-		id: 'kalilinux',
-		name: 'Kali Linux',
-		url: 'http://www.kali.org/'
-	};
 	var url = 'http://cdimage.kali.org/';
 	request.dom(url,function(err,$) {
 		if (err) { return callback(err); }
-		var files = $('a').map(function(a) {
+		var versionUrls = $('a').map(function(a) {
 			a = $(a);
 			return URL.resolve(url,a.attr('href'));
 		}).map(function(url) {
-			var match = /kali-linux-(\d+(?:\.\d+)*)-(\w+).*\.iso$/.exec(url);
-			if (!match) { return null; }
-			return {
-				url: url,
-				version: match[1],
-				arch: match[2]
-			};
+			if (/kali-\d+(\.\d+)*\/$/.test(url)) {
+				return url;
+			} else {
+				return null;
+			}
 		}).compact();
 
-		async.map(files, function(file,cb) {
-			request.contentlength(file.url,function(err,contentlength) {
-				file.size = contentlength;
-				cb(err,file);
+		async.reduce(versionUrls,[],function(releases,versionUrl,cb) {
+			request.dom(versionUrl,function(err,$) {
+				if (err) { return cb(err); }
+				async.map($('a').map(function(a) {
+						a = $(a);
+						return URL.resolve(versionUrl,a.attr('href'));
+					}).map(function(url) {
+						var match = /kali-linux-(\d+(?:\.\d+)*)-(\w+).*\.iso$/.exec(url);
+						if (!match) { return null; }
+						return {
+							url: url,
+							version: match[1],
+							arch: match[2]
+						};
+					}).compact(),
+				function(release,cb) {
+					request.contentlength(release.url,function(err,contentlength) {
+						release.size = contentlength;
+						cb(err,release);
+					});
+				},function(err,versionReleases) {
+					if (err) { return cb(err); }
+					cb(null,releases.concat(versionReleases));
+				});
 			});
-		}, function(err,files) {
-			distribution.releases = files;
-			callback(err,distribution);
+		},function(err,releases) {
+			if (err) { return callback(err); }
+			callback(err,{
+				id: 'kalilinux',
+				name: 'Kali Linux',
+				url: 'http://www.kali.org/',
+				releases: releases
+			});
 		});
 	});
 };
