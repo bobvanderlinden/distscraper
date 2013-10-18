60477b7fc9ff72ae6f3894b0af0f5574e6e12b7b
FrozenCow <frozencow@gmail.com>
2013-10-18 16:51:06 +0200

    Split to allow multiple repositories of scrapers

--- index.js
+++ index.js
@@ -3,139 +3,24 @@ var fs = require('fs');
 var async = require('async');
 var path = require('path');
 var request = require('./request');
-function debug() {
-	//console.log.apply(console,arguments);
+var program = require('commander');
+
+var scrapers = [];
+
+function includeRepository(name) {
+	fs.readdirSync(name).map(function(scraperName) {
+		return path.join(name,scraperName);
+	}).forEach(includeScraper);
 }
 
-function parallel() {
-	var pending = 0;
-	var errors = [];
-	var results = [];
-	var hasError = false;
-	var callback = null;
-	function create(innercallback) {
-		var myindex = pending;
-		errors.push(undefined);
-		results.push(undefined);
-		pending++;
-		return function(err,result) {
-			if (innercallback) {
-				innercallback(err,result);
-			}
-			if (err) { hasError = true; }
-			errors[myindex] = err;
-			results[myindex] = result;
-			pending--;
-			if (pending === 0) {
-				done();
-			}
-		};
-	}
-	function done() {
-		if (!hasError) {
-			errors = null;
-		} else {
-			errors = errors.filter(function(error) { return !!error; });
-		}
-		callback(errors,results);
-	}
-	create.done = function(cb) {
-		callback = cb;
-		if (pending === 0) {
-			done();
-		}
-	};
-	return create;
+function includeScraper(name) {
+	scrapers.push(require('./' + name));
 }
 
-function tryMatch(regexp,str) {
-	return (regexp.exec(str) || [])[0];
-}
-
-function filter_empty(e) { return e && e.length > 0; }
-
-function assertNoError(err) {
-	if (err) { onError(err); }
-}
-
-function onError(err) {
-	console.error('Error:',err);
-	console.error(new Error().stack);
-	process.exit(1);
-}
-
-function startRepl(c) {
-	var r = repl.start({});
-	r.context = c;
-}
-
-function sequential(arrayoffunctions,callback) {
-	function next() {
-		var f = arrayoffunctions.shift();
-		if (f) {
-			f(fcallback);
-		} else if (callback) {
-			callback();
-		}
-	}
-	function fcallback(err/*,...*/) {
-		if (err) { return callback(err); }
-		next();
-	}
-	next();
-}
-
-function sequentialForEach(arr,itercallback,donecallback) {
-	function next() {
-		var arg = arr.shift();
-		if (arg) {
-			itercallback(arg,next);
-		} else {
-			donecallback();
-		}
-	}
-	next();
-}
-
-function intersection(arrA,arrB) {
-	return arrA.filter(function(b) {
-		return arrB.indexOf(b) >= 0;
-	});
-}
-
-Array.prototype.concatArray = function(arr) {
-	Array.prototype.push.apply(this,arr);
-	return this;
-};
-
-Array.prototype.flatten = function() {
-	return this.reduce(function(a,b) {
-		return a.concat(b);
-	});
-};
-
-function getAllScrapers(callback) {
-	var scrapersPath = path.join(__dirname,'scrapers');
-	fs.readdir(scrapersPath,function(err,files) {
-		if (err) { return callback(err); }
-		files = files.map(function(file) { return path.join(scrapersPath,file); });
-		files.sort();
-		callback(null,files);
-	});
-}
-
-function getScrapers(scrapers) {
-	var scrapersPath = path.join(__dirname,'scrapers');
-	var scraperPaths = scrapers.map(function(file) { return path.join(scrapersPath,file); });
-	return function(callback) {
-		callback(null,scraperPaths);
-	};
-}
-
-function loadScrapers(scrapers,callback) {
-	var scrapers = scrapers.map(function(scraperName) { return require(scraperName); });
-	callback(null,scrapers);
-}
+program
+	.option('-r, --repository <repository>', 'Include repository',includeRepository)
+	.option('-s, --scraper <scraper>', 'Include scraper',includeScraper)
+	.parse(process.argv);
 
 function scrape(scrapers,callback) {
 	async.map(scrapers,function(scraper,callback) {
@@ -143,14 +28,9 @@ function scrape(scrapers,callback) {
 	},callback);
 }
 
-var scrapers = process.argv.slice(2);
-async.waterfall([
-	scrapers.length === 0 ? getAllScrapers : getScrapers(scrapers),
-	loadScrapers,
-	scrape
-],function(err,distributions) {
+scrape(scrapers, function(err,distributions) {
 	if (err) { console.error(err); throw err; }
-	console.log(JSON.stringify(distributions.compact()));
+	process.stdout.write(JSON.stringify(distributions.compact()));
 
 	var errors = validateDistributions(distributions);
 	if (errors.length > 0) {
