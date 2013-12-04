ffa506da39758a948cb1f8778853b221dceb8fca
FrozenCow <frozencow@gmail.com>
2013-12-04 14:34:15 +0100

    Fixed geexbox crawling versions multiple times

--- scrapers/geexbox.js
+++ scrapers/geexbox.js
@@ -1,11 +1,11 @@
 var async = require('async');
 var sugar = require('sugar');
 
-function first(a) { return a[0]; }
+function first(a) { return a[1]; }
 module.exports = function(request,callback) {
 	var distributionurl = 'http://download.geexbox.org/releases/';
 	request.dom(distributionurl,function(err,$) {
-		var versions = $('pre a').map(function(a) { return (/^\d+\.\d+/).exec(a.attr('href')); }).compact().map(first);
+		var versions = $('pre a').map(function(a) { return (/^(\d+\.\d+.*)\/$/).exec(a.attr('href')); }).compact().map(first);
 		var distribution = {
 			id: 'geexbox',
 			name: 'GeeXboX',
