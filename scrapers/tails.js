5cde76f2658090ece69ce76cce2757af27d6b8b9
FrozenCow <frozencow@gmail.com>
2014-04-18 14:14:35 +0200

    Use a single mirror for tails instead of round-robin.
    One (or more) of the mirrors did not return the expected page content.

--- scrapers/tails.js
+++ scrapers/tails.js
@@ -3,29 +3,27 @@ var async = require('async');
 var sugar = require('sugar');
 
 module.exports = function(request,cb) {
-	request.dom('http://dl.amnesia.boum.org/tails/stable/',function(err,$,response) {
-		var versionUrls = $('pre a').map(function(anchor) {
-			var url = $(anchor).attr('href');
-			return /^tails-(\w+)-(\d+(\.\d+)*)/.test(url) && url;
-		}).filter(function(url) {
-			return url;
-		}).map(function(url) {
-			return URL.resolve(response.url,url);
-		});
+	request.dom('http://212.192.64.234/tails/stable/',function(err,$,response) {
+		var versionUrls = $('pre a')
+			.map(function(anchor) {
+				var url = $(anchor).attr('href');
+				return /^tails-(\w+)-(\d+(\.\d+)*)/.test(url) && url;
+			})
+			.filter(function(url) { return url; })
+			.map(function(url) { return URL.resolve(response.url,url); });
 		async.map(versionUrls,function(versionUrl,cb) {
 			request.dom(versionUrl,function(err,$,response) {
 				if (err) { return cb(err); }
-				var releases = $('pre a').map(function(anchor) {
-					return /^tails-(\w+)-(\d+(\.\d+)*)\.iso$/.exec(anchor.attr('href'));
-				}).filter(function(match) {
-					return match !== null;
-				}).map(function(match) {
-					return {
-						url: URL.resolve(response.url,match[0]),
-						version: match[2],
-						arch: match[1]
-					};
-				});
+				var releases = $('pre a')
+					.map(function(anchor) { return /^tails-(\w+)-(\d+(\.\d+)*)\.iso$/.exec(anchor.attr('href')); })
+					.filter(function(match) { return match !== null; })
+					.map(function(match) {
+						return {
+							url: URL.resolve(response.url,match[0]).replace('212.192.64.234','dl.amnesia.boum.org'),
+							version: match[2],
+							arch: match[1]
+						};
+					});
 				async.map(releases,function(release,cb) {
 					request.contentlength(release.url,function(err,contentLength) {
 						if (err) { return cb(err); }
