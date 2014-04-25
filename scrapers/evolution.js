6c3591d578020fc11c7eef2c245c157dc8589d0f
FrozenCow <frozencow@gmail.com>
2014-04-25 17:56:15 +0200

    Added Evo/Lution

---
+++ scrapers/evolution.js
@@ -0,0 +1,36 @@
+var async = require('async');
+var sugar = require('sugar');
+var URL = require('url');
+
+module.exports = function(request,cb) {
+	var url = 'http://sourceforge.net/projects/evolutionlinux/files/';
+	request.dom(url,function(err,$,response) {
+		if (err) { return cb(err); }
+		var urls = $('th[headers=files_name_h] a.name').toArray()
+			.map(function(a) { return URL.resolve(response.url,$(a).attr('href'))
+				.replace(/^https/,'http')
+				.replace(/\/download$/,'');
+			})
+			.filter(function(url) { return (/\.iso$/).test(url); });
+		async.map(urls,function(url,cb) {
+			var version = /\-(\d+(\.\d+)*(\-\d+[a-z]*)?)/.exec(url)[1];
+			request.contentlength(url,function(err,size) {
+				if (err) { return cb(err); }
+				cb(null,{
+					url: url,
+					version: version,
+					size: size
+				});
+			});
+		},function(err,releases) {
+			if (err) { return cb(err); }
+			cb(null,{
+				id: 'evolution',
+				name: 'Evo/Lution',
+				tags: ['hybrid'],
+				url: 'http://sourceforge.net/projects/evolutionlinux/',
+				releases: releases
+			});
+		});
+	});
+};
