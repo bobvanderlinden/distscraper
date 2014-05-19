3b0ca5672b5d9ef470a3d4a18da2783ea0f9a709
FrozenCow <frozencow@gmail.com>
2014-05-19 11:33:27 +0200

    tails: change mirror Tor (and ignore the invalid certificates)

--- scrapers/tails.js
+++ scrapers/tails.js
@@ -2,8 +2,18 @@ var URL = require('url');
 var async = require('async');
 var sugar = require('sugar');
 
+function requestOptions(url) {
+	return {
+		url: url,
+		headers: {
+			host: 'dl.amnesia.boum.org'
+		},
+		rejectUnauthorized: true
+	};
+}
+
 module.exports = function(request,cb) {
-	request.dom('http://212.192.64.234/tails/stable/',function(err,$,response) {
+	request.dom(requestOptions('http://83.212.104.246/tails/stable/'),function(err,$,response) {
 		var versionUrls = $('pre a')
 			.map(function(anchor) {
 				var url = $(anchor).attr('href');
@@ -12,20 +22,20 @@ module.exports = function(request,cb) {
 			.filter(function(url) { return url; })
 			.map(function(url) { return URL.resolve(response.url,url); });
 		async.map(versionUrls,function(versionUrl,cb) {
-			request.dom(versionUrl,function(err,$,response) {
+			request.dom(requestOptions(versionUrl),function(err,$,response) {
 				if (err) { return cb(err); }
 				var releases = $('pre a')
 					.map(function(anchor) { return /^tails-(\w+)-(\d+(\.\d+)*)\.iso$/.exec(anchor.attr('href')); })
 					.filter(function(match) { return match !== null; })
 					.map(function(match) {
 						return {
-							url: URL.resolve(response.url,match[0]).replace('212.192.64.234','dl.amnesia.boum.org'),
+							url: URL.resolve(response.url,match[0]).replace('83.212.104.246','dl.amnesia.boum.org'),
 							version: match[2],
 							arch: match[1]
 						};
 					});
 				async.map(releases,function(release,cb) {
-					request.contentlength(release.url,function(err,contentLength) {
+					request.contentlength(requestOptions(release.url),function(err,contentLength) {
 						if (err) { return cb(err); }
 						release.size = contentLength;
 						cb(null,release);
