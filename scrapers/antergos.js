5d4b0cf3afcfc036b9169b573e4552945ff4955b
FrozenCow <frozencow@gmail.com>
2016-06-07 10:16:39 +0200

    antergos: fixed for forbidden directory listing

--- scrapers/antergos.js
+++ scrapers/antergos.js
@@ -1,35 +1,65 @@
 var async = require('async');
 var sugar = require('sugar');
-var URL = require('url');
+var URL = require('../lib/url');
+var Rx =require('../lib/rxnode');
+var request =require('../lib/rxrequest');
 
-function first(a) { return a[1]; }
-module.exports = function(request,cb) {
-    request.dom('http://mirrors.antergos.com/iso/release/',function(err,$,response) {
-        var releases = $('a')
-            .map(function(a) { return (/^antergos-(\d+(?:\.\d+)*)-(i686|x86_64)\.iso$/).exec(a.attr('href')); })
-            .compact()
-            .map(function(match) {
-                return {
-                    url: URL.resolve(response.url, match[0]),
-                    version: match[1],
-                    arch: match[2]
-                };
-            })
-        async.map(releases,function(release,cb) {
-            request.contentlength(release.url,function(err,contentlength) {
-                if (err) { return cb(err); }
-                release.size = contentlength;
-                return cb(null,release);
-            });
-        },function(err,releases) {
-            if (err) { return cb(err); }
-            return cb(null,{
+module.exports = function(_,cb) {
+    request.dom('https://antergos.com/download/')
+        .flatMap(function($) {
+            return $('h2.entry-title a')
+                .map(function(a) {
+                    return {
+                        title: $(a).text().trim(),
+                        url: $(a).attr('href')
+                    };
+                });
+        })
+        .filter(function(post) {
+            return /ISO Refresh/.test(post.title);
+        })
+        .flatMap(function(post) {
+            return request.dom(post.url);
+        })
+        .flatMap(function($) {
+            return $('a')
+                .map(function(a) { return $(a).attr('href'); });
+        })
+        .filter(function(url) {
+            return /\.iso\.md5$/.test(url);
+        })
+        .distinct()
+        .map(function(url) {
+            return /^https?:\/\/.*\/antergos-\w+-(\d{4}.\d{2}.\d{2})-(x86_64|i686).iso/.exec(url)
+        })
+        .filter(function(match) {
+            return match;
+        })
+        .map(function(match) {
+            return {
+                url: match[0],
+                version: match[1],
+                arch: match[2]
+            };
+        })
+        .flatMap(function(release) {
+            return request.contentlength(release.url)
+                .filter(function(contentlength) { return contentlength; })
+                .map(function(contentlength) {
+                    return Object.merge(release, {
+                        size: contentlength
+                    });
+                });
+        })
+        .toArray()
+        .map(function(releases) {
+            return {
                 id: 'antergos',
                 name: 'Antergos',
                 tags: ['hybrid'],
                 url: 'http://www.antergos.com/',
-                releases: releases.flatten()
-            });
-        });
-    });
+                releases: releases
+            };
+        })
+        .subscribeCallback(cb);
 };
