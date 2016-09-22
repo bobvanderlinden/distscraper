d080fe367527bde4fe83864ec029c4c50c24198d
FrozenCow <frozencow@gmail.com>
2016-09-22 16:56:35 +0200

    fixed usage of catch by returning an observable instead of value itself

--- index.js
+++ index.js
@@ -241,10 +241,10 @@ Rx.Observable.from(scrapers)
 		console.log('Resolving', distribution.id,'...');
 	})
 	.concatMap(distribution => resolveDistributionReleases(distribution)
-		.catch(err => Object.merge(distribution, {
+		.catch(err => Observable.just(Object.merge(distribution, {
 			error: err,
 			releases: []
-		}))
+		})))
 	)
 	.doOnNext(distribution => {
 		if (distribution.error) {
