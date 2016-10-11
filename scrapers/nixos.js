8a10371d58ca88d5ed5b22018caee2363f679b1d
FrozenCow <frozencow@gmail.com>
2016-10-11 11:21:18 +0200

    nixos: cleanup

--- scrapers/nixos.js
+++ scrapers/nixos.js
@@ -5,15 +5,10 @@ var Rx =require('../lib/rxnode');
 var request =require('../lib/rxrequest');
 var filelisting = require('../lib/sites/filelisting');
 
-function log(o) {
-  console.log('log', o);
-  return o;
-}
-
 module.exports = function(_,cb) {
   filelisting.getEntries('https://nixos.org/channels/')
-    .filter(function(entry) { return /^nixos-\d+\.\d+(-small)?/.test(entry.name); })
-    .flatMap(function(entry) { return request.dom(entry.url); })
+    .filter(entry => /^nixos-\d+\.\d+(-small)?/.test(entry.name))
+    .flatMap(entry => request.dom(entry.url))
     .flatMap($ =>
       $('a')
         .map(anchor => $(anchor))
@@ -23,11 +18,9 @@ module.exports = function(_,cb) {
           url: URL.resolve($.response.request.uri.href, anchor.attr('href'))
         }))
     )
-    .map(function(entry) { return entry.url; })
-    .filter(function(url) {
-      return /\.iso$/.test(url);
-    })
-    .map(function(url) {
+    .map(entry => entry.url)
+    .filter(url => /\.iso$/.test(url))
+    .map(url => {
       var match = /nixos(-graphical|-minimal|)-(\d+\.\d+\.\d+\.\w+)-(\w+)-linux.iso$/g.exec(url);
       return {
         url: url.replace(/^https:/, 'http:'),
@@ -35,23 +28,16 @@ module.exports = function(_,cb) {
         version: match[2]
       };
     })
-    .flatMap(function(release) {
-      return request.contentlength(release.url)
-        .map(function(contentLength) {
-          return Object.merge(release, {
-            size: contentLength
-          });
-        });
-    })
+    .flatMap(release => request.contentlength(release.url)
+      .map(contentLength => Object.merge(release, { size: contentLength }))
+    )
     .toArray()
-    .map(function(releases) {
-      return {
-        id: 'nixos',
-        name: 'NixOS',
-        tags: ['hybrid'],
-        url: 'http://nixos.org/',
-        releases: releases
-      };
-    })
+    .map(releases => ({
+      id: 'nixos',
+      name: 'NixOS',
+      tags: ['hybrid'],
+      url: 'http://nixos.org/',
+      releases: releases
+    }))
     .subscribeCallback(cb);
 };
