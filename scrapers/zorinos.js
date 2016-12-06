4f239a9128e9037169d84ffc96fe8b4dc56ba0e3
FrozenCow <frozencow@gmail.com>
2016-12-06 17:52:38 +0100

    zorinos: converted to es6

--- scrapers/zorinos.js
+++ scrapers/zorinos.js
@@ -1,49 +1,39 @@
 var async = require('async');
 var sugar = require('sugar');
 var URL = require('../lib/url');
-var Rx =require('../lib/rxnode');
-var request =require('../lib/rxrequest');
+var Rx = require('../lib/rxnode');
+var request = require('../lib/rxrequest');
 var sourceforge = require('../lib/sites/sourceforge');
 
-module.exports = function(_,cb) {
+module.exports = function (_, cb) {
   var sourceforgeProject = sourceforge.project('zorin-os');
   sourceforgeProject.files()
-    .filter(function(entry) { return entry.type === 'directory'; })
-    .flatMap(function(entry) { return sourceforgeProject.files(entry.path); })
-    .filter(function(entry) { return entry.type === 'file'; })
-    .map(function(entry) { return entry.url; })
-    .map(function(url) {
-      return url
-        .replace(/^https/,'http')
-        .replace(/\/download$/, '');
-    })
-    .filter(function(url) {
-      return /\.iso$/.test(url);
-    })
-    .map(function(url) {
-      return {
-        url: url,
-        arch: (/32|64/.exec(url) || ['32'])[0],
-        version: /zorin\-os\-(\d+(\.\d+)*)\-/g.exec(url)[1]
-      };
-    })
-    .flatMap(function(release) {
-      return request.contentlength(release.url)
-        .map(function(contentLength) {
-          return Object.merge(release, {
-            size: contentLength
-          });
-        });
-    })
+    .filter(entry => entry.type === 'directory')
+    .flatMap(entry => sourceforgeProject.files(entry.path))
+    .filter(entry => entry.type === 'file')
+    .map(entry => entry.url)
+    .map(url => url
+      .replace(/^https/, 'http')
+      .replace(/\/download$/, '')
+    )
+    .filter(url => /\.iso$/.test(url))
+    .map(url => ({
+      url: url,
+      arch: (/32|64/.exec(url) || ['32'])[0],
+      version: /zorin\-os\-(\d+(\.\d+)*)\-/g.exec(url)[1]
+    }))
+    .flatMap(release => request.contentlength(release.url)
+      .map(contentLength => Object.merge(release, {
+        size: contentLength
+      }))
+    )
     .toArray()
-    .map(function(releases) {
-      return {
-        id: 'zorinos',
-        name: 'Zorin OS',
-        tags: ['hybrid'],
-        url: 'http://zorinos.com/',
-        releases: releases
-      };
-    })
+    .map(releases => ({
+      id: 'zorinos',
+      name: 'Zorin OS',
+      tags: ['hybrid'],
+      url: 'http://zorinos.com/',
+      releases: releases
+    }))
     .subscribeCallback(cb);
 };
