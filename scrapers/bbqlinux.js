f312e2e080704f5cfe8f218c9d76172b3983c44c
FrozenCow <frozencow@gmail.com>
2018-02-11 19:35:32 +0100

    bbqlinux: use sourceforge library

--- scrapers/bbqlinux.js
+++ scrapers/bbqlinux.js
@@ -1,50 +1,36 @@
 var async = require('async');
 var sugar = require('sugar');
-var URL = require('url');
+var URL = require('../lib/url');
+var Rx =require('../lib/rxnode');
+var request =require('../lib/rxrequest');
+const sourceforge = require('../lib/sites/sourceforge');
 
-module.exports = function(request,cb) {
-	var url = 'http://sourceforge.net/projects/bbqlinux/files/';
-	request.dom(url,function(err,$,response) {
-		if (err) { return cb(err); }
-		var folders = $('th[headers=files_name_h] a.name')
-			.map(function(a) { return URL.resolve(response.url,$(a).attr('href')); });
-		async.concat(folders,function(url,cb) {
-			request.dom(url,function(err,$,response) {
-				if (err) { return cb(err); }
-				async.map($('th[headers=files_name_h] a.name').toArray(),function(a,cb) {
-					a = $(a);
-					var url = URL.resolve(response.url,a.attr('href'))
-						.replace(/^https/,'http')
-						.replace(/\/download$/,'');
-					if (!/\.iso$/.test(url)) {
-						return cb(null,null);
-					}
-					var arch = /i\d86|x86_64/.exec(url)[0];
-					var version = /\d{4}\.\d{2}\.\d{2}/.exec(url)[0];
-					request.contentlength(url,function(err,size) {
-						if (err) { return cb(err); }
-						cb(null,{
-							url: url,
-							arch: arch,
-							version: version,
-							size: size
-						});
-					});
-				},function(err,releases) {
-					if (err) { return cb(err); }
-					cb(null,releases.compact());
-				});
-			});
-		},function(err,releases) {
-			if (err) { return cb(err); }
-			cb(null,{
-				id: 'bbqlinux',
-				name: 'BBQLinux',
-				tags: ['hybrid'],
-				url: 'http://bbqlinux.org/',
-				tags: ['hybrid'],
-				releases: releases
-			});
-		});
-	});
+module.exports = function(_,callback) {
+	var project = sourceforge.project('bbqlinux');
+	project.files('x86_64')
+		.filter(entry => entry.type === 'file')
+		.map(entry => {
+			const match = /^bbqlinux-(\d+(?:\.\d+)*)-(amd64|x86_64|i386|i686)-(?:\w+).iso$/.exec(entry.name)
+			if (!match) { return; }
+			return {
+				url: entry.url,
+				arch: match[2],
+				version: match[1],
+			};
+    })
+    .filter(release => release)
+    .flatMap(release => request.contentlength(release.url)
+      .map(contentLength => Object.merge(release, {
+        size: contentLength
+      }))
+    )
+    .toArray()
+    .map(releases => ({
+			id: 'bbqlinux',
+			name: 'BBQLinux',
+			tags: ['hybrid'],
+			url: 'http://bbqlinux.org/',
+      releases: releases
+    }))
+    .subscribeCallback(callback);
 };
