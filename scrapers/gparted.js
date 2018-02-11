761a0b9b39b0b50282a6151ad4d127f09879d554
FrozenCow <frozencow@gmail.com>
2018-02-11 19:35:48 +0100

    gparted: use sourceforge library

--- scrapers/gparted.js
+++ scrapers/gparted.js
@@ -1,55 +1,35 @@
-var async = require('async');
-var sugar = require('sugar');
-var URL = require('url');
+var Rx =require('../lib/rxnode');
+var request =require('../lib/rxrequest');
+const sourceforge = require('../lib/sites/sourceforge');
 
-function first(a) { return a[0]; }
-module.exports = function(request,callback) {
-	var distribution = {
-		id: 'gparted',
-		name: 'GParted',
-		tags: ['hybrid'],
-		url: 'http://gparted.sourceforge.net/'
-	};
-	var url = 'http://sourceforge.net/projects/gparted/files/gparted-live-stable/';
-	request.dom(url,function(err,$) {
-		if (err) { return callback(err); }
-		var versions = $('th[headers=files_name_h] a.name').map(function(a) {
-			a = $(a);
-			return { url: URL.resolve(url,a.attr('href')), name: a.text().replace(/\s+/g,'') };
-		}).filter(function(version) {
-			return /^(\d+(\.\d+)*(-\d+)?)$/g.test(version.name);
-		});
-		async.map(versions,function(version,callback) {
-			var url = version.url;
-			request.dom(url,function(err,$) {
-				if (err) { return callback(err); }
-				var files = $('th[headers=files_name_h] a.name').map(function(a) {
-					a = $(a);
-					var fileUrl = URL.resolve(url,a.attr('href'))
-						.replace(/^https/,'http')
-						.replace(/\/download$/,'');
-					return { url: fileUrl, version: version.name };
-				}).filter(function(file) {
-					return /\.iso$/.test(file.url);
-				}).map(function(file) {
-					var archMatch = /(i486|i686|amd64)/.exec(file.url);
-					if (archMatch) {
-						file.arch = archMatch[0];
-					}
-					return file;
-				});
-				async.map(files,function(file,callback) {
-					request.contentlength(file.url,function(err,contentLength) {
-						if (err) { return callback(err); }
-						file.size = contentLength;
-						callback(null,file);
-					});
-				},callback);
-			});
-		},function(err,files) {
-			if (err) { return callback(err); }
-			distribution.releases = files.flatten();
-			callback(null,distribution);
-		});
-	});
+module.exports = function(_,callback) {
+  var project = sourceforge.project('gparted');
+	project.files('gparted-live-stable')
+		.filter(entry => entry.type === 'directory')
+		.flatMap(entry => project.files(entry.path))
+    .filter(entry => entry.type === 'file')
+		.map(entry => {
+			const match = /^gparted-live-(\d+(?:\.\d+)+(?:-\d+)?)-(x86_64|i386|i686|amd64)(?:-\w+).iso$/.exec(entry.name)
+			if (!match) { return; }
+			return {
+				url: entry.url,
+				arch: match[2],
+				version: match[1],
+			};
+    })
+    .filter(release => release)
+    .flatMap(release => request.contentlength(release.url)
+      .map(contentLength => Object.merge(release, {
+        size: contentLength
+      }))
+    )
+    .toArray()
+    .map(releases => ({
+      id: 'gparted',
+      name: 'GParted',
+      tags: ['hybrid'],
+      url: 'https://gparted.org/',
+      releases: releases
+    }))
+    .subscribeCallback(callback);
 };
