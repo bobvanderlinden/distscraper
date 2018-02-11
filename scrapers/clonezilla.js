d040c50476d44a5f2fb1991bd72f3c60a862fe16
FrozenCow <frozencow@gmail.com>
2018-02-11 19:35:40 +0100

    clonezilla: use sourceforge library

--- scrapers/clonezilla.js
+++ scrapers/clonezilla.js
@@ -1,52 +1,41 @@
 var async = require('async');
 var sugar = require('sugar');
-var URL = require('url');
+var URL = require('../lib/url');
+var Rx =require('../lib/rxnode');
+var request =require('../lib/rxrequest');
+const sourceforge = require('../lib/sites/sourceforge');
 
-function first(a) { return a[0]; }
-module.exports = function(request,callback) {
-	var distribution = {
-		id: 'clonezilla',
-		name: 'Clonezilla',
-		tags: ['hybrid'],
-		url: 'http://clonezilla.org/'
-	};
-	var url = 'http://sourceforge.net/projects/clonezilla/files/clonezilla_live_stable/';
-	request.dom(url,function(err,$) {
-		if (err) { return callback(err); }
-		var versions = $('th[headers=files_name_h] a.name').map(function(a) {
-			a = $(a);
-			return { url: URL.resolve(url,a.attr('href')), name: a.text().replace(/\s+/g,'') };
-		}).filter(function(version) {
-			return /^(\d+(\.\d+)*(-\d+)?)$/g.test(version.name);
-		});
-		async.map(versions,function(version,callback) {
-			var url = version.url;
-			request.dom(url,function(err,$) {
-				if (err) { return callback(err); }
-				var files = $('th[headers=files_name_h] a.name').map(function(a) {
-					a = $(a);
-					var fileUrl = URL.resolve(url,a.attr('href'))
-						.replace(/^https/,'http')
-						.replace(/\/download$/,'');
-					return { url: fileUrl, version: version.name };
-				}).filter(function(file) {
-					return /\.iso$/.test(file.url);
-				}).map(function(file) {
-					file.arch = /(i486|i586|i686|amd64)/.exec(file.url)[0];
-					return file;
-				});
-				async.map(files,function(file,callback) {
-					request.contentlength(file.url,function(err,contentLength) {
-						if (err) { return callback(err); }
-						file.size = contentLength;
-						callback(null,file);
-					});
-				},callback);
-			});
-		},function(err,files) {
-			if (err) { return callback(err); }
-			distribution.releases = files.flatten();
-			callback(null,distribution);
-		});
-	});
+module.exports = function(_,callback) {
+	var project = sourceforge.project('clonezilla');
+	Rx.Observable.merge(
+		project.files('clonezilla_live_stable'),
+		project.files('clonezilla_live_alternative')
+	)
+		.filter(entry => entry.type === 'directory')
+		.flatMap(entry => project.files(entry.path))
+		.filter(entry => entry.type === 'file')
+		.map(entry => {
+			const match = /^clonezilla-live-(\d+(?:\.\d+)*)-(?:\w+)-(amd64|x86_64|i386|i686).iso$/.exec(entry.name)
+			if (!match) { return; }
+			return {
+				url: entry.url,
+				arch: match[2],
+				version: match[1],
+			};
+    })
+    .filter(release => release)
+    .flatMap(release => request.contentlength(release.url)
+      .map(contentLength => Object.merge(release, {
+        size: contentLength
+      }))
+    )
+    .toArray()
+    .map(releases => ({
+			id: 'clonezilla',
+			name: 'Clonezilla',
+			tags: ['hybrid'],
+			url: 'https://clonezilla.org/',
+      releases: releases
+    }))
+    .subscribeCallback(callback);
 };
