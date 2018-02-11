b333af157106b9e8a33d094741588acb8ee2c3d9
FrozenCow <frozencow@gmail.com>
2018-02-11 20:20:09 +0100

    trisquel: use filelisting library

--- scrapers/trisquel.js
+++ scrapers/trisquel.js
@@ -1,37 +1,35 @@
 var async = require('async');
 var sugar = require('sugar');
+var URL = require('../lib/url');
+var Rx = require('../lib/rxnode');
+var request = require('../lib/rxrequest');
+var filelisting = require('../lib/sites/filelisting');
 
-module.exports = function(request,callback) {
-	var distributionurl = 'http://mirror.fsf.org/trisquel-images/';
-	request.dom(distributionurl,function(err,$) {
-		if (err) { return callback(err); }
-		var releases = $('a').map(function(a) {
-			return (/^trisquel(?:-\w+)?_(\d+(?:\.\d+)*)_(amd64|i686).iso$/).exec(a.attr('href'));
-		}).compact().map(function(match) {
-			return {
-				url: distributionurl+match[0],
-				version: match[1],
-				arch: match[2]
-			};
-		});
-		var distribution = {
-			id: 'trisquel',
-			name: 'Trisquel',
-			tags: ['hybrid'],
-			url: 'http://trisquel.info/'
-		};
-
-		async.map(releases,function(release,callback) {
-			request.contentlength(release.url,function(err,contentLength) {
-				if (err) { return callback(err); }
-				release.size = contentLength;
-				callback(null,release);
-			});
-		},function(err,releases) {
-			if (err) { return callback(err); }
-			distribution.releases = releases;
-			callback(null,distribution);
-		});
-	});
+module.exports = function (_, cb) {
+  filelisting.getEntries('https://mirror.fsf.org/trisquel-images/')
+    .filter(entry => entry.type === 'file')
+    .filter(entry => /\.iso$/.test(entry.url))
+    .map(entry => {
+			var match = /trisquel(-\w+)?_(\d+(?:\.\d+)*)_(amd64|i686).iso$/g.exec(entry.url);
+			if (!match) { return; }
+      return {
+        url: entry.url,
+        arch: entry[3],
+        version: match[2]
+      };
+		})
+		.filter(release => release)
+    .flatMap(release => request.contentlength(release.url)
+      .map(contentLength => Object.merge(release, { size: contentLength }))
+    )
+    .toArray()
+    .map(releases => ({
+      id: 'trisquel',
+      name: 'Trisquel',
+      tags: ['hybrid'],
+      url: 'https://trisquel.info/',
+      releases: releases
+    }))
+    .subscribeCallback(cb);
 };
 
