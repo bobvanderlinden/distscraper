34efc0cfdf9274610a40fc542352400ec5683ca5
eater <hello@eaterofco.de>
2019-05-14 11:32:01 +0200

    Fix repository location of VoidLinux and display all versions and fix version string

--- scrapers/voidlinux.js
+++ scrapers/voidlinux.js
@@ -3,15 +3,15 @@ var request = require('../lib/rxrequest');
 var filelisting = require('../lib/sites/filelisting');
 
 module.exports = function (_, cb) {
-  filelisting.getEntries('https://repo.voidlinux.eu/live/current/')
+  filelisting.getEntries('https://alpha.de.repo.voidlinux.org/live/current/')
     .filter(entry => entry.type === 'file')
     .map(entry => {
-      const match = /^void-live-(i686)-(\d{8})(?:-\w+)?\.iso$/g.exec(entry.name)
+      const match = /^void-live-(i686|x86_64)-(?:(?:(musl)-)?(\d{4,})(\d{2})(\d{2})(?:-(\w+))?)\.iso$/g.exec(entry.name)
       if (!match) return null
       return {
         url: entry.url,
         arch: match[1],
-        version: match[2]
+        version: `${match[6] ? `${match[6]} ` : ''}(${match[2] ? `${match[2]} ` : ''}${match[3]}-${match[4]}-${match[5]})`
       }
     })
     .filter(entry => entry)
